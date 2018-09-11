483cf55e3466bd8d2663936ace6af4fe72c2f360
1185956753 <1185956753@qq.com>
2018-09-11 14:05:03 +0800

    init_Homework_4

---
+++ Chapter1_Homework_Programmingquestion4/Chapter1_Homework_Programmingquestion4/Chapter1_Homework_Programmingquestion4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Chapter1_Homework_Programmingquestion4
+{
+    public class Chapter1_Homework_Programmingquestion4
+    {
+        public static void Main(string[] args)
+        {
+            //定义变量
+            string s1 = "";
+            string s2 = "";
+            int n1, n2, product;
+            //提示输入数字，接受并将字符（串）转化为整型数字
+            Console.Write("Please input a number:");
+            s1 = Console.ReadLine();
+            n1 = Int32.Parse(s1);
+            Console.Write("Please input another number:");
+            s2 = Console.ReadLine();
+            n2 = Int32.Parse(s2);
+            //计算乘积并且输出
+            product = n1 * n2;
+            Console.Write($"The product of two number is {product}");
+        }
+    }
+}
